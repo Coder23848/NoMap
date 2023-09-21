c9cf32ac8d310ab41240b5d5dabbd94c98f64328
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2023-09-21 19:18:47 -0400

    Add project files.

---
+++ src/plugin/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace NoMap
+{
+    [BepInPlugin("com.coder23848.nomap", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+#pragma warning disable IDE0051 // Visual Studio is whiny
+        private void OnEnable()
+#pragma warning restore IDE0051
+        {
+            // Plugin startup logic
+            On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+            On.HUD.Map.Update += Map_Update;
+            On.HUD.Map.Draw += Map_Draw;
+            On.Menu.FastTravelScreen.ctor += FastTravelScreen_ctor;
+            On.Menu.FastTravelScreen.Update += FastTravelScreen_Update;
+            On.RWInput.PlayerInput += RWInput_PlayerInput;
+        }
+
+        // Suppress the map button entirely while on the region/Passage screen.
+        bool suppressMapButton = false;
+        private void FastTravelScreen_Update(On.Menu.FastTravelScreen.orig_Update orig, Menu.FastTravelScreen self)
+        {
+            suppressMapButton = true;
+            orig(self);
+            suppressMapButton = false;
+        }
+        private Player.InputPackage RWInput_PlayerInput(On.RWInput.orig_PlayerInput orig, int playerNumber, RainWorld rainWorld)
+        {
+            Player.InputPackage result = orig(playerNumber, rainWorld);
+            if (suppressMapButton)
+            {
+                result.mp = false;
+            }
+            return result;
+        }
+
+        // Forcing map fade to 0 removes the "open map" background effects in select/shelter/region/Passage screens.
+        private void Map_Update(On.HUD.Map.orig_Update orig, HUD.Map self)
+        {
+            orig(self);
+            self.fade = 0;
+        }
+
+        // Remove the prompts in the Regions/Passage screen telling you to open the map.
+        private void FastTravelScreen_ctor(On.Menu.FastTravelScreen.orig_ctor orig, Menu.FastTravelScreen self, ProcessManager manager, ProcessManager.ProcessID ID)
+        {
+            orig(self, manager, ID);
+            self.mapButtonPrompt.text = "";
+            self.mapButtonPrompt.label.Redraw(false, false);
+        }
+
+        // Prevents the map from showing up.
+        private void Map_Draw(On.HUD.Map.orig_Draw orig, HUD.Map self, float timeStacker)
+        {
+            // Nothing
+        }
+
+        private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig(self);
+            Debug.Log("No Map config setup: " + MachineConnector.SetRegisteredOI(PluginInfo.PLUGIN_GUID, PluginOptions.Instance));
+        }
+    }
+}
